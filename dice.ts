5c4c741b02dd55e8364b2f633404e68ff435b089
Jonas Schiffl <jonas.schiffl@kit.edu>
2024-04-03 10:08:34 +0200

    now there's five of them

--- dice.ts
+++ dice.ts
@@ -1,15 +1,13 @@
 document.getElementById('roll-dice')?.addEventListener('click', () => {
-    const result: number = Math.floor(Math.random() * 6) + 1;
-    const resultElement = document.getElementById('result');
-    if (resultElement) {
-        resultElement.textContent = 'You rolled a ' + result;
-    }
+    const results: number[] = Array.from({length: 5}, () => Math.floor(Math.random() * 6) + 1);
 
-    const resultImgElement = document.getElementById('resultImg') as HTMLImageElement;
-    if (resultImgElement) {
-        const imgSrc = 'img/testdice' + result + '.png';
-        resultImgElement.src = imgSrc;
-    } else {
-        console.log('No img element found');
-    }
+    results.forEach((result, index) => {
+        const resultImgElement = document.getElementById('resultImg' + (index + 1)) as HTMLImageElement;
+        if (resultImgElement) {
+            const imgSrc = 'img/testdice' + result + '.png';
+            resultImgElement.src = imgSrc;
+        } else {
+            console.log('No img element found for dice ' + (index + 1));
+        }
+    });
 });
