7cd1652ac09a32a92c3fd434d56110d12f3b39a4
Jonas <jonas-schiffl@hotmail.de>
2024-04-09 17:36:50 +0200

    dice and images for everybody

--- dice.ts
+++ dice.ts
@@ -1,34 +1,20 @@
-
-document.getElementById('roll-dice')?.addEventListener('click', () => {
-    const results: number[] = Array.from({length: 5}, () => Math.floor(Math.random() * 6) + 1);
-
-    results.forEach((result, index) => {
-        const resultImgElement = document.getElementById('resultImg' + (index + 1)) as HTMLImageElement;
-        if (resultImgElement) {
-            const imgSrc = 'img/testdice' + result + '.png';
-            resultImgElement.src = imgSrc;
-        } else {
-            console.log('No img element found for dice ' + (index + 1));
-        }
-    });
-
-    createPlayerSections(5);
-});
-
 document.getElementById('setup-game')?.addEventListener('click', () => {
     console.log('Starting game...');
 
     const numPlayersForm = document.getElementById('num-players-form') as HTMLFormElement;
 
     const numPlayers = Number((numPlayersForm.elements.namedItem('num-players') as RadioNodeList).value);
-    const diceVals: number[][] TODO
-
+    // a two-dimensional array of dice values for each player
+    const diceVals: number[][] = Array.from({length: numPlayers}, () => 
+                                    Array.from({length: 5}, () => Math.floor(Math.random() * 6) + 1));
     console.log('Number of players: ' + numPlayers);
-    createPlayerSections(numPlayers);
+    createPlayerSections(numPlayers, diceVals[0]);
+    const p1section = document.getElementById('player1');
     numPlayersForm.style.display = 'none';
+    document.getElementById('setup-game').style.display = 'none';
 });
 
-function createPlayerSections(numPlayers) {
+function createPlayerSections(numPlayers: number, p1dice: number[]) {
     const container = document.getElementById('player-container');
     for (let i = 1; i <= numPlayers; i++) {
         const playerSection = document.createElement('section');
@@ -46,6 +32,8 @@ function createPlayerSections(numPlayers) {
             playerSection.appendChild(resultImg);
             if (i != 1) {
                 resultImg.src = 'img/quma.png';
+            } else {
+                resultImg.src = 'img/testdice' + p1dice[j-1] + '.png';
             }
         }
 
